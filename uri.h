6870c5c2f2c7b247a82181c3be25f99789b305df
Ilja Kartashov <ik@lowenware.com>
2017-11-07 11:18:52 +0100

    uri module establishment

--- uri.h
+++ uri.h
@@ -1,28 +1,11 @@
-#ifndef _STANDARD_URI_H_
-#define _STANDARD_URI_H_
+#ifndef _CSTUFF_URI_H_
+#define _CSTUFF_URI_H_
 
-#include <stdbool.h>
-#include <stdint.h>
+#ifdef CSTUFF_WITH_URI_DECODE
 
-typedef struct
-{
-  int16_t protocol_len;
-  int16_t user;
-  int16_t user_len;
-  int16_t password;
-  int16_t password_len;
-  int16_t address;
-  int16_t address_len;
-  int16_t port;
-  int16_t port_len;
-  int16_t path;
-  int16_t path_len;
-  int16_t query;
-  int16_t query_len;
-} uri_t;
+void
+uri_decode(char *data);
 
-
-bool
-uri_parse(const char *in, int in_len, uri_t *out);
+#endif
 
 #endif
